75b8be2bcd5086204ebd962d20c7cf55d1057808
Stefan Burri <stefanburri@gmx.net>
2018-01-03 16:53:23 +0100

    topic 1 ready without checking

---
+++ inc/assets/js/application.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    
+    $('.fs-toggler').click(function() {
+       $(this).closest('.fs-toggle-group').toggleClass('active');
+        return false;
+    });
+    
+    
+});
