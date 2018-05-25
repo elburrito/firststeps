2dca2d68ddf4fe564a7b46af160e61daa6b2a807
Stefan Burri <stefan@elburrito.ch>
2018-05-25 13:10:56 +0200

    waypoint js now works. checkbox & attentionseeker elements added

--- dist/assets/js/application.js
+++ dist/assets/js/application.js
@@ -1,35 +1,46 @@
-$(document).ready(function() {
-    /**
-    toggles everything
-    */
-    $('.fs-toggler').click(function() {
-       $(this).closest('.fs-toggle-group').toggleClass('active');
-        return false;
-    });
+$(document).ready(function()
+{
+  /**
+  toggles everything
+  */
+  $('.fs-toggler').click(function()
+  {
+    $(this).closest('.fs-toggle-group').toggleClass('active');
+    return false;
+  });
 
 
-    /**
-    CHAPTER 02 - specific stuff
-    */
-    $('#tp02_selfcheck .toggle.btn').click(function() {
-        setTimeout(function() {
-            //console.log($('#tp02_selfcheck .toggle.btn.btn-primary').length);
-            if($('#tp02_selfcheck .toggle.btn.btn-primary').length > 0) {
-                $('#selfcheck-result').html('Im Konflikt mit Gott').addClass("conflict");
-            } else {
-                $('#selfcheck-result').html('Im Einklang mit Gott').removeClass("conflict");;
-            }
-        },1000);
-
-        return;
-    });
-
-    var waypointChapterCheckbox = new Waypoint({
-      element: document.getElementById('Syav9Sm17'),
-      handler: function(direction) {
-        //$(this.element).removeClass('.animated-onviewport').addClass('animated');
-        console.log('Scrolled to checkbox!');
+  /**
+  CHAPTER 02 - specific stuff
+  */
+  $('#tp02_selfcheck .toggle.btn').click(function()
+  {
+    setTimeout(function()
+    {
+      //console.log($('#tp02_selfcheck .toggle.btn.btn-primary').length);
+      if ($('#tp02_selfcheck .toggle.btn.btn-primary').length > 0)
+      {
+        $('#selfcheck-result').html('Im Konflikt mit Gott').addClass("conflict");
       }
-      //offset: 20
+      else
+      {
+        $('#selfcheck-result').html('Im Einklang mit Gott').removeClass("conflict");;
+      }
+    }, 1000);
+
+    return;
+  });
+  $('#waypoint_chapter_finished').waypoint(function(direction)  {
+    $('#waypoint_chapter_finished').removeClass('.animated-onviewport').addClass('animated');
+    }, {
+      offset: '60%'
     });
+
+
+    $('.fs-attentionseeker').waypoint(function(direction) {
+      $(this.element).addClass('pulse-once');
+    }, {
+      offset: '80%'
+    })
+
 });
